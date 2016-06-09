bcc03158f3004498a1068964a0bb358d882de93c
Stephen Ryan <sryan@dyn.com>
2016-06-09 15:04:57 -0400

    Add unfill-paragraph command

--- init.el
+++ init.el
@@ -1004,6 +1004,20 @@ With numeric prefix arg DEC, decrement the integer by DEC amount."
     )
 )
 
+; From http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
+
 
 ;; End let from the top
 )
