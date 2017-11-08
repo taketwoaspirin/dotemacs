f00a4763650bce49cc279e907642aa8e950ef563
Stephen Ryan <taketwoaspirin@gmail.com>
2017-11-07 21:15:16 -0500

    Didn't really like z / fz /fzf first go-round.

--- bin/setup_bash.sh
+++ bin/setup_bash.sh
@@ -16,25 +16,3 @@ if [ $? == 1 ] ; then
     echo 'source ~/.config/up/up.sh' >> ~/.bashrc
 fi
 
-# fzf
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# z
-
-mkdir ~/.bash_completion.d
-curl "https://raw.githubusercontent.com/rupa/z/master/{z.sh}" -o ~/.bash_completion.d/"#1"
-
-# fz
-
-curl "https://raw.githubusercontent.com/changyuheng/fz/master/{fz.sh}" -o ~/.bash_completion.d/z"#1"
-
-grep "~/.bash_completion.d" ~/.bashrc
-if [ $? == 1 ] ; then
-    echo "if [ -d ~/.bash_completion.d ] ; then" >> ~/.bashrc
-    echo "  for file in ~/.bash_completion.d/* ; do" >> ~/.bashrc
-    echo "    source $file" >> ~/.bashrc
-    echo "  done" >> ~/.bashrc
-    echo "fi" >> ~/.bashrc
-fi
